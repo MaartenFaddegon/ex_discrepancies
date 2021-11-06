f5c3f073a2621fc3227c5b3ed16f2ed2ff080006
Maarten Faddegon <github@maartenfaddegon.nl>
2021-11-06 15:05:19 +0100

    first

---
+++ lib/ex_discrepancies.ex
@@ -0,0 +1,18 @@
+defmodule ExDiscrepancies do
+  @moduledoc """
+  Documentation for `ExDiscrepancies`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDiscrepancies.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_discrepancies_test.exs
@@ -0,0 +1,5 @@
+defmodule ExDiscrepanciesTest do
+  use ExUnit.Case
+  doctest ExDiscrepancies
+
+end
